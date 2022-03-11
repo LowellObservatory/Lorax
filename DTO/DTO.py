dc7305f5f64a072aa3100ece3f92decba23d9233
Dyer Lytle <dlytle@lowell.edu>
2022-03-11 16:15:59 -0700

    Added rudimentary DTO

---
+++ DTO/DTO.py
@@ -0,0 +1,121 @@
+"""
+Created on March 11, 2022
+
+@author: dlytle
+
+"""
+
+import time
+import logging
+import stomp
+import yaml
+
+# Set stomp so it only logs WARNING and higher messages. (default is DEBUG)
+logging.getLogger("stomp").setLevel(logging.WARNING)
+
+
+class DTO:
+    hosts = ""
+    log_file = ""
+
+    def __init__(self):
+
+        # Read the config file.
+        with open("configure.yaml", "r") as stream:
+            try:
+                self.config = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        # Get the log file name from the configuration.
+        # Set up the logger.
+        self.log_file = self.config["log_file"]
+        logging.basicConfig(
+            filename=self.log_file,
+            format="%(asctime)s %(levelname)-8s %(message)s",
+            level=logging.DEBUG,
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        self.dto_logger = logging.getLogger("pw_mount_log")
+
+        # Tell em we've started.
+        self.dto_logger.info("Initializing: logging started")
+
+        # Get the broker host from the configuration.
+        # Make a connection to the broker.
+        self.hosts = [tuple(self.config["broker_hosts"])]
+        self.dto_logger.info(
+            "connecting to broker at " + str(self.config["broker_hosts"])
+        )
+
+        try:
+            # Get a connection handle.s
+            self.conn = stomp.Connection(host_and_ports=self.hosts)
+
+            # Set up a listener and and connect.
+            self.conn.set_listener("", self.MyListener(self))
+            self.conn.connect(wait=True)
+        except:
+            self.dto_logger.error("Connection to broker failed")
+
+        self.dto_logger.info("connected to broker")
+        self.dto_logger.info(
+            "subscribing to topic: " + self.config["mount_incoming_topic"]
+        )
+
+        # Subscribe to messages from "mount_incoming_topic"
+        # and "camera_incoming_topic".
+        self.conn.subscribe(
+            id=1,
+            destination="/topic/" + self.config["mount_incoming_topic"],
+            headers={},
+        )
+
+        self.dto_logger.info(
+            "subscribed to topic " + self.config["mount_incoming_topic"]
+        )
+
+        self.dto_logger.info(
+            "subscribing to topic: " + self.config["camera_incoming_topic"]
+        )
+
+        self.conn.subscribe(
+            id=1,
+            destination="/topic/" + self.config["camera_incoming_topic"],
+            headers={},
+        )
+
+        self.dto_logger.info(
+            "subscribed to topic " + self.config["camera_incoming_topic"]
+        )
+
+    def send_hello(self):
+        self.conn.send(
+            body="Hello World",
+            destination="/topic/" + self.config["mount_incoming_topic"],
+        )
+        time.sleep(2)
+
+    class MyListener(stomp.ConnectionListener):
+        def __init__(self, parent):
+            self.parent = parent
+            pass
+
+        def on_error(self, message):
+            print('received an error "%s"' % message)
+
+        def on_message(self, message):
+            # print('received a message "%s"' % message)
+            self.parent.dto_logger.info('received a message "%s"' % message.body)
+            # self.parent.planewave_mount_talk.send_command_to_mount(message.body)
+
+
+if __name__ == "__main__":
+    dto = DTO()
+    dto.send_hello()
+
+"""
+Loop {
+    If command is exit
+        exit
+} """
