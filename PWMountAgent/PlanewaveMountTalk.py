f0aa1ffe75b9040973c9231b9e40be0d58498b91
Dyer Lytle <dlytle@lowell.edu>
2022-05-11 08:46:55 -0700

    autostart PWI4

--- PWMountAgent/PlanewaveMountTalk.py
+++ PWMountAgent/PlanewaveMountTalk.py
@@ -5,6 +5,10 @@ Created on Feb 7, 2022
 """
 
 from extern.pwi4_client import PWI4
+import psutil
+import subprocess
+import time
+import os
 
 
 class PlanewaveMountTalk(object):
@@ -13,8 +17,29 @@ class PlanewaveMountTalk(object):
     """
 
     def __init__(self, parent, host, port):
-        self.pwi4 = PWI4(host=host, port=port)
+        # self.pwi4 = PWI4(host=host, port=port)
         self.parent = parent
+        if self.checkIfProcessRunning("run-pwi4"):
+            print("true")
+            self.pwi4 = PWI4(host=host, port=port)
+        else:
+            print("false")
+            os.environ["DISPLAY"] = ":6.1"
+            subprocess.call("./run-pwi4", cwd="/home/lorax/PWI4/pwi-4.0.11beta10")
+            time.sleep(10)
+            self.pwi4 = PWI4(host=host, port=port)
+
+    def checkIfProcessRunning(self, processName):
+
+        # Iterate over the all the running process
+        for proc in psutil.process_iter():
+            try:
+                # Check if process name contains the given name string.
+                if processName in proc.name():
+                    return True
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        return False
 
     def connect_to_mount(self):
         s = self.pwi4.status()
